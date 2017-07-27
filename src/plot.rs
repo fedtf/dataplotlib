f70d289784191815db5a9e93785417ba9e41452a
fedtf <fed.tf@yandex.ru>
2017-07-27 21:00:40 +0300

    single-thread multi-window SDL GUI works

--- src/plot.rs
+++ src/plot.rs
@@ -9,16 +9,179 @@ use sdl2::pixels;
 use sdl2::keyboard::Keycode;
 
 use sdl2::gfx::primitives::DrawRenderer;
-use sdl2::video::Window;
+use sdl2::rect::Point;
+use sdl2::render::Renderer;
 
 use std::cmp::min;
 use std::time::Duration;
 use std::{mem, thread, f64};
+use std::sync::Mutex;
+use std::sync::Arc;
+use std::collections::HashMap;
+use std::collections::VecDeque;
 
 use plotbuilder::*;
 
 
 pub struct Plot {
+}
+
+pub enum GUITask {
+    PlotTask(PlotBuilder2D, Plot),
+    Terminate,
+}
+
+pub struct PlotGUI<'a> {
+    sdl_context: sdl2::Sdl,
+    pub windows: HashMap<u32, Window<'a>>,
+}
+
+pub struct Window<'a>{
+    id: u32,
+    renderer: Renderer<'a>,
+    xs: Option<Vec<Vec<f64>>>,
+    ys: Option<Vec<Vec<f64>>>,
+    colors: Option<Vec<[f32; 4]>>,
+    plot_bounds: Option<[f64; 4]>,
+}
+
+trait Surface {
+    fn draw_plots(&mut self, xs: Vec<Vec<f64>>, ys: Vec<Vec<f64>>, colors: Vec<[f32; 4]>, plot_bounds: [f64; 4]);
+}
+
+impl<'a> PlotGUI<'a> {
+    pub fn run(task_queue: Arc<Mutex<VecDeque<GUITask>>>) -> thread::JoinHandle<()> {
+        thread::spawn(|| PlotGUI::event_loop(task_queue))
+    }
+
+    fn event_loop(task_queue: Arc<Mutex<VecDeque<GUITask>>>) {
+        let sdl_context = sdl2::init().unwrap();
+        let mut plot_gui = PlotGUI{sdl_context, windows: HashMap::new()};
+
+        let mut events = plot_gui.sdl_context.event_pump().unwrap();
+
+        'main: loop {
+            for event in events.poll_iter() {
+                match event {
+                    Event::Quit { .. } => break 'main,
+                    Event::KeyDown { keycode: Some(Keycode::Escape), window_id, .. } 
+                    | Event::Window { win_event: sdl2::event::WindowEvent::Close, window_id, .. } => {
+                        if let Some(_) = plot_gui.windows.remove(&window_id) {
+                            if plot_gui.windows.is_empty() {
+                                break 'main;
+                            }
+                        }
+                    },
+                    _ => {}
+                }
+            }
+            loop {
+                match task_queue.lock().unwrap().pop_front() {
+                    Some(gui_task) => {
+                        match gui_task {
+                            GUITask::PlotTask(plot_builder, plot) => plot_gui.add_window(plot_builder,
+                                                                                         plot),
+                            GUITask::Terminate  => if plot_gui.windows.is_empty() {break 'main},
+                        }
+                    }
+                    None => break
+                }
+            }
+
+            for window in plot_gui.windows.values_mut() {
+                window.draw();
+            }
+            thread::sleep(Duration::from_millis(500));
+        }       
+    }
+
+    fn add_window(&mut self, plot_builder: PlotBuilder2D, plot: Plot) {
+        let sdl_video = self.sdl_context.video().unwrap();
+        let window = sdl_video.window("2D plot", 720, 720)
+            .position_centered()
+            .resizable()
+            .opengl()
+            .build()
+            .unwrap();
+        let id = window.id();
+        let mut renderer = window.renderer().build().unwrap();
+        let mut new_window = Window::new(id, renderer);
+        plot.new2d(plot_builder, &mut new_window);
+        self.windows.insert(id, new_window);
+    }
+}
+
+impl<'a> Window<'a> {
+    fn new(id: u32, renderer: Renderer<'a>) -> Window {
+        Window{id, renderer, xs: None, ys: None, colors: None, plot_bounds: None}
+    }
+
+    fn draw(&mut self) {
+        let xs = self.xs.as_ref().unwrap();
+        let ys = self.ys.as_ref().unwrap();
+        let plot_bounds = self.plot_bounds.as_ref().unwrap();
+        let colors = self.colors.as_ref().unwrap();
+
+        let bordercol = f32_4_to_color([0.95, 0.95, 0.95, 1.0]);
+        let bgcol = f32_4_to_color([1.0, 1.0, 1.0, 1.0]);
+        let margin = 0.05;
+        let invmargin = 1.0 - margin;
+
+        let x_max = plot_bounds[0];
+        let y_max = plot_bounds[1];
+        let x_min = plot_bounds[2];
+        let y_min = plot_bounds[3];
+
+        let (mut w, mut h) = self.renderer.output_size().unwrap();
+
+        // println!("(w, h) = ({}, {})", w, h);
+        let m = min(w, h) as f64;
+        let space = m * margin;
+        let m = m * invmargin;
+
+        self.renderer.set_draw_color(bgcol);
+        self.renderer.clear();
+        draw_borders(bordercol, bgcol, space, m, &mut self.renderer);
+
+        let y0 = (m + space) as i16 - point2plot(0.0, y_min, y_max, m, space);
+        let xn = m;
+        // println!("xn: {}", xn);
+        self.renderer.thick_line(space as i16,
+                                 y0,
+                                 xn as i16,
+                                 y0,
+                                 2,
+                                 pixels::Color::RGBA(0, 0, 0, 255))
+                     .unwrap();
+
+        for i in 0..colors.len() {
+            let color = colors[i];
+            let color_rgba = f32_4_to_color(color);
+
+            let y_inv = (m + space) as i16;
+            let yt: Vec<i16> = ys[i].iter().map(|y| y_inv - point2plot(*y, y_min, y_max, m, space)).collect();
+            let xt: Vec<i16> = xs[i].iter().map(|x| point2plot(*x, x_min, x_max, m, space)).collect();
+
+            // The number of points
+            let len = xs[i].len();
+            for i in 0..len - 1 {
+                let (xa, ya) = (xt[i + 0], yt[i + 0]);
+                let (xb, yb) = (xt[i + 1], yt[i + 1]);
+                self.renderer.thick_line(xa, ya, xb, yb, 2, color_rgba).unwrap();
+            }
+        }
+        self.renderer.present();
+    }
+}
+
+impl<'a> Surface for Window<'a> {
+    fn draw_plots(&mut self, xs: Vec<Vec<f64>>, ys: Vec<Vec<f64>>, colors: Vec<[f32; 4]>, plot_bounds: [f64; 4]) {
+        self.xs = Some(xs);
+        self.ys = Some(ys);
+        self.colors = Some(colors);
+        self.plot_bounds = Some(plot_bounds);
+        self.draw();
+    }
 }
 
 // pt: a point on a 1 dimensional line segment
@@ -65,7 +228,7 @@ fn f32_4_to_color(col: [f32; 4]) -> pixels::Color {
                         (col[3] * 255f32) as u8)
 }
 
-fn draw_borders(bordercol: pixels::Color, bgcol: pixels::Color, space: f64, m: f64, renderer: &mut sdl2::render::Renderer) {
+fn draw_borders(bordercol: pixels::Color, bgcol: pixels::Color, space: f64, m: f64, renderer: &mut Renderer) {
     renderer.set_draw_color(bordercol);
     renderer.clear();
 
@@ -96,88 +259,6 @@ fn set_xy(xy: &Vec<(f64, f64)>, x_vector: &mut Vec<Vec<f64>>, y_vector: &mut Vec
     }
 }
 
-fn draw_plots(sdl_context: sdl2::Sdl, window: Window, xs: &Vec<Vec<f64>>, ys: &Vec<Vec<f64>>, colors: &Vec<[f32; 4]>, plot_bounds: [f64; 4]) {
-    let bordercol = f32_4_to_color([0.95, 0.95, 0.95, 1.0]);
-    let bgcol = f32_4_to_color([1.0, 1.0, 1.0, 1.0]);
-    let margin = 0.05;
-    let invmargin = 1.0 - margin;
-
-    let x_max = plot_bounds[0];
-    let y_max = plot_bounds[1];
-    let x_min = plot_bounds[2];
-    let y_min = plot_bounds[3];
-
-    let mut renderer = window.renderer().build().unwrap();
-    let (mut w, mut h) = renderer.output_size().unwrap();
-
-    let mut events = sdl_context.event_pump().unwrap();
-
-    let mut update_frame = |w, h| {
-        // println!("(w, h) = ({}, {})", w, h);
-        let m = min(w, h) as f64;
-        let space = m * margin;
-        let m = m * invmargin;
-
-        renderer.set_draw_color(bgcol);
-        renderer.clear();
-        draw_borders(bordercol, bgcol, space, m, &mut renderer);
-
-        let y0 = (m + space) as i16 - point2plot(0.0, y_min, y_max, m, space);
-        let xn = m;
-        println!("xn: {}", xn);
-        renderer.thick_line(space as i16,
-                        y0,
-                        xn as i16,
-                        y0,
-                        2,
-                        pixels::Color::RGBA(0, 0, 0, 255))
-            .unwrap();
-
-        for i in 0..colors.len() {
-            let color = colors[i];
-            let color_rgba = f32_4_to_color(color);
-
-            let y_inv = (m + space) as i16;
-            let yt: Vec<i16> = ys[i].iter().map(|y| y_inv - point2plot(*y, y_min, y_max, m, space)).collect();
-            let xt: Vec<i16> = xs[i].iter().map(|x| point2plot(*x, x_min, x_max, m, space)).collect();
-
-            // The number of points
-            let len = xs[i].len();
-            for i in 0..len - 1 {
-                let (xa, ya) = (xt[i + 0], yt[i + 0]);
-                let (xb, yb) = (xt[i + 1], yt[i + 1]);
-                renderer.thick_line(xa, ya, xb, yb, 2, color_rgba).unwrap();
-            }
-        }
-        renderer.present();
-    };
-
-    update_frame(w, h);
-
-    'main: loop {
-        for event in events.poll_iter() {
-            match event {
-                Event::Quit { .. } => break 'main,
-
-                Event::KeyDown { keycode: Some(keycode), .. } => {
-                    if keycode == Keycode::Escape {
-                        break 'main;
-                    }
-                }
-                Event::Window { win_event: sdl2::event::WindowEvent::Resized(new_w, new_h), .. } => {
-                    w = new_w as u32;
-                    h = new_h as u32;
-                    update_frame(w, h);
-                }
-                _ => {}
-            }
-        }
-
-        thread::sleep(Duration::from_millis(30));
-
-    }
-}
-
 fn get_plot_bounds(plot_builder: &PlotBuilder2D, xs: &Vec<Vec<f64>>, ys: &Vec<Vec<f64>>) -> [f64; 4] {
 
     let mut max_xs: Vec<f64> = Vec::new();
@@ -204,20 +285,13 @@ fn get_plot_bounds(plot_builder: &PlotBuilder2D, xs: &Vec<Vec<f64>>, ys: &Vec<Ve
 }
 
 impl Plot {
-    pub fn new2d(plot_builder: PlotBuilder2D) {
-
-        let sdl_context = sdl2::init().unwrap();
-        let video_subsys = sdl_context.video().unwrap();
-        let window = video_subsys.window("2D plot", 720, 720)
-            .position_centered()
-            .resizable()
-            .opengl()
-            .build()
-            .unwrap();
+    pub fn new() -> Plot {
+        Plot{}
+    }
+    
+    pub fn new2d(&self, plot_builder: PlotBuilder2D, surface: &mut Surface) {
 
         let mut plot_builder = plot_builder;
-
-        // window.set_ups(60);
 
         let mut pvs = Vec::new();
 
@@ -239,11 +313,6 @@ impl Plot {
 
         // [MAX_X, MAX_Y, MIN_X, MIN_Y]
         let plot_bounds: [f64; 4] = get_plot_bounds(&plot_builder, &x_points, &y_points);
-        draw_plots(sdl_context,
-                   window,
-                   &x_points,
-                   &y_points,
-                   &colors,
-                   plot_bounds);
+        surface.draw_plots(x_points, y_points, colors, plot_bounds);
     }
 }
--- src/plotter.rs
+++ src/plotter.rs
@@ -5,33 +5,57 @@
 //! For now, `Plotter::plot2d` is the only supported plotting function. It takes a `PlotBuilder2D` containing all needed information.
 //!
 //! The `Plotter::join` function allows the thread that owns the `Plotter` to wait until the user has closed all open plot windows before continuing.
-
 use std::thread;
+use std::sync::Mutex;
+use std::sync::Arc;
+use std::collections::VecDeque;
+
 use plotbuilder::PlotBuilder2D;
 use plot::Plot;
+use plot::GUITask;
+use plot::PlotGUI;
+
+use std::sync::atomic::{AtomicBool, ATOMIC_BOOL_INIT, Ordering};
+static GUI_INITIALISED: AtomicBool = ATOMIC_BOOL_INIT;
 
 pub struct Plotter {
-    plots: Vec<thread::JoinHandle<()>>,
+    plot_gui: Option<thread::JoinHandle<()>>,
+    task_queue: Arc<Mutex<VecDeque<GUITask>>>,
 }
 
 impl Plotter {
     /// `new` creates a new `Plotter` object to manage asynchronous plots
-    pub fn new() -> Plotter {
-        Plotter { plots: Vec::new() }
+    pub fn new() -> Result<Plotter, String> {
+        let task_queue = Arc::new(Mutex::new(VecDeque::new()));
+        
+        if GUI_INITIALISED.load(Ordering::Relaxed) {
+            Err("Only one gui Plotter may be initialised at a time.".to_owned())
+        }
+        else {
+            GUI_INITIALISED.store(true, Ordering::Relaxed);
+            Ok(Plotter { 
+                plot_gui: Some(PlotGUI::run(task_queue.clone())),
+                task_queue,
+            })
+        }
     }
 
     /// `plot2d` is currently the only supported plotting function. It takes a `PlotBuilder2D` containing all needed information.
-    pub fn plot2d(&mut self, plotbuilder: PlotBuilder2D) {
-        self.plots.push(thread::spawn(move || {
-            Plot::new2d(plotbuilder);
-        }));
+    pub fn plot2d(&mut self, plot_builder: PlotBuilder2D) {
+        let plot_task = GUITask::PlotTask(plot_builder, Plot::new());
+        self.task_queue.lock().unwrap().push_back(plot_task);
     }
 
-    /// The `join` function allows the thread that owns the `Plotter` to wait until the user has closed all open plot windows before continuing.
-    pub fn join(self) {
-        for t in self.plots {
-            let _ = t.join();
+}
+
+impl Drop for Plotter {
+    fn drop(&mut self) {
+        if let Some(plot_gui) = self.plot_gui.take() {
+            // terminate the gui thread if there are no plots open and wait for it
+            self.task_queue.lock().unwrap().push_back(GUITask::Terminate);
+            plot_gui.join().unwrap();
         }
+        GUI_INITIALISED.store(false, Ordering::Relaxed);
     }
 }
 
